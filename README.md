ced48ac17204fb1c14c6331d28f750c32d3a85f5
ivan.rey.c.v <ivan.rey.c.v@gmail.com>
2018-06-14 12:08:12 +0800

    Initialize repo with create-react-app, remove unnecessary files

---
+++ README.md
@@ -0,0 +1,1 @@
+A PWA Tailwind css cheat sheet.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<main className="App">
+				<h1>Hello from React!</h1>
+			</main>
+		);
+	}
+}
